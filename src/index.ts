fa784d6b678b1ac6ea402e9851232255eee29b63
Marvin Zeising <marvin.zeising@gmail.com>
2021-04-19 20:55:12 +0200

    1.0.2 update index typings

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-export * from './lib/authProvider'
-export * from './lib/constants'
+export { AuthProvider } from './lib/authProvider'
+export {
+  MS365_ID_TOKEN,
+  MS365_ACCESS_TOKEN,
+  MS365_TOKEN_EXPIRY_TIMESTAMP,
+  MS365_SIGN_IN_EVENT,
+  MS365_SIGN_IN_CANCELLED_EVENT,
+  MS365_SIGN_OUT_EVENT,
+} from './lib/constants'
